6d51b4466f07bbc0a6f3e8c04c856008227fbdf9
emmanuel <blimanuevil@gmail.com>
2023-01-10 20:31:45 +0100

    initialisation du projet creation des composants Navbar, Navlinks et Hero

---
+++ components/navbar/navbar.js
@@ -0,0 +1,28 @@
+import Image from 'next/image'
+
+import Navlinks from '../navlinks/navlinks'
+import styles from './navbar.module.css'
+
+export default function Navbar() {
+    return (
+        <div className={styles.container}>
+            <Image
+                className={styles.logo} 
+                src="/images/logo.svg"
+                height={24}
+                width={146}
+                priority={true}
+                alt=""
+            />
+            <Image
+                className={styles.burgerIcon} 
+                src="/images/icon-hamburger.svg"
+                height={18}
+                width={25}
+                priority={true}
+                alt=""
+            />
+            <Navlinks open={false} />
+        </div>
+    )
+}
---
+++ components/navlinks/navlinks.js
@@ -0,0 +1,15 @@
+import styles from './navlinks.module.css'
+
+export default function Navlinks({open}) {
+    return (
+        <nav className={styles.container}>
+            <ul>
+                <li></li> 
+                <li></li> 
+                <li></li> 
+                <li></li>
+                <li></li> 
+            </ul>
+        </nav>
+    )
+}
